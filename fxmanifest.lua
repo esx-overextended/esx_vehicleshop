3a94b1f5a701d5fddbe58c64d7abc6266aa4a3f7
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-07-21 16:15:45 -0400

    initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version "cerulean"
+use_experimental_fxv2_oal "yes"
+lua54 "yes"
+game "gta5"
+
+name "esx_vehicleshops"
+version "0.0.0"
+description "ESX-Overextended Vehicle Shops"
+
+dependencies {
+    "es_extended",
+    "ox_target"
+}
+
+shared_scripts {
+    "@es_extended/imports.lua",
+    "@ox_lib/init.lua",
+    "shared/*.lua",
+}
+
+server_scripts {
+    "@oxmysql/lib/MySQL.lua",
+    "server/*.lua"
+}
+
+client_scripts {
+    "client/*.lua",
+}
