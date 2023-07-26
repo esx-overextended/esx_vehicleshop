00946dab5fbb0633e3e83b5e882e84ecf692b804
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-07-25 22:53:03 -0400

    tweak(server): show warning on invalid vehicle models

--- server/main.lua
+++ server/main.lua
@@ -3,6 +3,21 @@ local vehicles, categories
 function RefreshVehiclesAndCategories()
     vehicles = MySQL.query.await("SELECT * FROM vehicles")
     categories = MySQL.query.await("SELECT * FROM vehicle_categories")
+    local generatedVehicles = ESX.GetVehicleData()
+    local validVehicles, validVehiclesCount = {}, 0
+
+    for i = 1, #vehicles do
+        local vehicleData = vehicles[i]
+
+        if not generatedVehicles[vehicleData?.model] then
+            ESX.Trace(("Vehicle (^5%s^7) with the model of (^1%s^7) is ^1NOT KNOWN^7 to the framework!\nEither it's an invalid model or has not been parsed/generated yet!\n"):format(vehicleData?.name, vehicleData?.model), "warning", true)
+        else
+            validVehiclesCount += 1
+            validVehicles[validVehiclesCount] = vehicleData
+        end
+    end
+
+    vehicles = validVehicles
 end
 
 function GetVehiclesAndCategories()
